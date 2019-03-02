da453f9a6d8d4e10d757d3812e449627653f11cd
Martin Hlavacka <mato.hlavacka@gmail.com>
2019-03-02 09:11:48 +0100

    [TRAINING] Updated functionality: - added visualizing for fft - each record, even same exercise is now separated - output and input name - to be fixed - num_labels

--- py-train/convert.py
+++ py-train/convert.py
@@ -8,7 +8,7 @@ import argparse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import stats, signal
+from scipy import signal
 import tensorflow as tf
 
 tf.logging.set_verbosity(tf.logging.ERROR)
@@ -37,15 +37,9 @@ if args.fourier is True and args.visualize is False:
     parser.error('-v is required when -f is set.')
 
 
-def feature_normalize(to_normalize):
-    mu = np.mean(to_normalize, axis=0)
-    sigma = np.std(to_normalize, axis=0)
-    return (to_normalize - mu) / sigma
-
-
-def plot_axis(ax, x, y, title):
+def plot_activity_axis(ax, x, y, title):
     ax.plot(x, y)
-    ax.set_title(title)
+    ax.set_title("")  # (title)
     ax.xaxis.set_visible(False)
     ax.set_ylim([min(y) - np.std(y), max(y) + np.std(y)])
     ax.set_xlim([min(x), max(x)])
@@ -55,62 +49,64 @@ def plot_axis(ax, x, y, title):
 def plot_activity(name, x, y, z):
     fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(15, 10), sharex=True)
     length = len(x)
-    plot_axis(ax0, range(length), x, 'x-axis')
-    plot_axis(ax1, range(length), y, 'y-axis')
-    plot_axis(ax2, range(length), z, 'z-axis')
+    plot_activity_axis(ax0, range(length), x, 'x-axis')
+    plot_activity_axis(ax1, range(length), y, 'y-axis')
+    plot_activity_axis(ax2, range(length), z, 'z-axis')
     plt.subplots_adjust(hspace=0.2)
     fig.suptitle(name)
     plt.subplots_adjust(top=0.90)
     plt.show()
 
 
-def plot_fft(name, X, idx, Y, idy, Z, idz):
-    lngth = X.size
-    W_x = np.fft.fft(X)
-    W_y = np.fft.fft(Y)
-    W_z = np.fft.fft(Z)
-    freq = np.fft.fftfreq(X.size, 1)
-    x = 1.0 / freq[:ceil(X.size / 2)]
+def plot_fft_axis(subplot, axis, id):
+    W = np.fft.fft(axis)
+    lngth = axis.size
+    freq = np.fft.fftfreq(axis.size, 1)
+    x = 1.0 / freq[:ceil(axis.size / 2)]
 
+    # subplot.yaxis.set_visible(False)
+    subplot.set_xlim(0, 80)
+    subplot.plot(lngth / x, abs(W[:ceil(lngth / 2)]))
+    subplot.scatter([lngth / (1 / abs(freq[id])), ], [np.abs(W[id]), ], s=100, color='r')
+
+
+def plot_fft(name, X, idx, Y, idy, Z, idz):
     fig, (ax, ay, az) = plt.subplots(nrows=3, figsize=(15, 10), sharex=True)
 
-    ax.set_title('x-axis')
-    ax.yaxis.set_visible(False)
-    ax.set_xlim(0, 80)
-    # plt.ylim(0, np.nanmax(x[x != np.inf]))
-    ax.plot(lngth / x, abs(W_x[:ceil(lngth / 2)]))
-    ax.scatter([lngth / (1 / abs(freq[idx])), ], [np.abs(W_x[idx]), ], s=100, color='r')
-
-    ay.set_title('y-axis')
-    ay.yaxis.set_visible(False)
-    ay.set_xlim(0, 80)
-    ay.plot(lngth / x, abs(W_y[:ceil(lngth / 2)]))
-    ay.scatter([lngth / (1 / abs(freq[idy])), ], [np.abs(W_y[idy]), ], s=100, color='r')
-
-    az.set_title('z-axis')
-    az.yaxis.set_visible(False)
-    az.set_xlim(0, 80)
-    az.plot(lngth / x, abs(W_z[:ceil(lngth / 2)]))
-    az.scatter([lngth / (1 / abs(freq[idz])), ], [np.abs(W_z[idz]), ], s=100, color='r')
+    plot_fft_axis(ax, X, idx)
+    plot_fft_axis(ay, Y, idy)
+    plot_fft_axis(az, Z, idz)
 
     plt.subplots_adjust(hspace=0.4)
+    fig.suptitle(name)
     plt.show()
 
 
-def estimate_period(df):
-    X = feature_normalize(df["x-axis"]).values
-    Y = feature_normalize(df["y-axis"]).values
-    Z = feature_normalize(df["z-axis"]).values
+def normalize_axis(input):
+    mu = np.mean(input, axis=0)
+    sigma = np.std(input, axis=0)
+    return (input - mu) / sigma
 
-    freq = np.fft.fftfreq(X.size, 1)
+
+def estimate_period(df):
+    X = normalize_axis(df["x-axis"]).values
+    Y = normalize_axis(df["y-axis"]).values
+    Z = normalize_axis(df["z-axis"]).values
+
     # Look for the longest signal that is "loud"
-    threshold = 100
-    idx = np.where(abs(np.fft.fft(X)) > threshold)[0][-1]
-    idy = np.where(abs(np.fft.fft(Y)) > threshold)[0][-1]
-    idz = np.where(abs(np.fft.fft(Z)) > threshold)[0][-1]
+    treshhold = 225
+    try:
+        idx = np.concatenate([[0], np.where(abs(np.fft.fft(X)) > treshhold)[0]])[-1]
+        idy = np.concatenate([[0], np.where(abs(np.fft.fft(Y)) > treshhold)[0]])[-1]
+        idz = np.concatenate([[0], np.where(abs(np.fft.fft(Z)) > treshhold)[0]])[-1]
+    except:
+        print("problem with " + df["activity"] + " -- " + df["y-axis"][0])
+        exit(1)
 
     if args.fourier:
         plot_fft(df["activity"].iloc[0], X, idx, Y, idy, Z, idz)
+
+    freq = np.fft.fftfreq(X.size, 1)
 
     estimations = [X.size / (1 / abs(freq[idx])), Y.size / (1 / abs(freq[idy])), Z.size / (1 / abs(freq[idz]))]
     est_filtered = [x for x in estimations if 3 < x < 21]
@@ -119,8 +115,10 @@ def estimate_period(df):
 
 def filter_and_estimate(data):
     all_max = 0
-    filtered_activities = dict()
-    indexes = data.ne(data.shift()).apply(lambda x: x.index[x].tolist()).values[0]
+    filtered_activities = []
+    indexes = [0] + list(
+        idx for idx, (i, j) in enumerate(zip(data['timestamp'], data['timestamp'][1:]), 1) if
+        (i + 1000 < j) or (i - 1000 > j))
     for i in range(len(indexes)):
         start = 0 if i == 0 else indexes[i] + 1
         if i + 1 < len(indexes):
@@ -142,7 +140,7 @@ def filter_and_estimate(data):
                 (filtered_activities[exercise_name][0].append(filtered),
                  filtered_activities[exercise_name][1] + estimation)
         else:
-            filtered_activities[exercise_name] = (filtered, estimation)
+            filtered_activities.append((exercise_name, filtered, estimation))
     return all_max, filtered_activities
 
 
@@ -153,11 +151,12 @@ def read_data_and_filter(file_path):
     return filter_and_estimate(data)
 
 
-def plot_activities_and_exit(max_len, filtered):
-    for activity, (filtered, estimation) in filtered.items():
-        plot_activity(activity, filtered['x-axis'], filtered['y-axis'], filtered['z-axis'])
-        chunks = estimation if estimation > 0 else ceil(len(filtered) / max_len)
+def plot_activities_and_exit(max_len, activities):
+    for activity, filtered, estimation in activities:
+        plot_activity(activity + "--" + str(estimation), filtered['x-axis'], filtered['y-axis'],
+                      filtered['z-axis'])
         if args.chunks:
+            chunks = estimation if estimation > 0 else ceil(len(filtered) / max_len)
             for chunk in np.array_split(filtered, chunks):
                 plot_activity(activity, chunk['x-axis'], chunk['y-axis'], chunk['z-axis'])
     exit(0)
@@ -188,20 +187,20 @@ def plot_images_and_exit(length, activities):
 
 
 def segment_signal(max_len, filtered_activities):
-    l_labels = np.empty(0)
-    l_segments = np.empty((0, max_len, 3))
+    labels = np.empty(0)
+    segments = np.empty((0, max_len, 3))
 
-    for activity, (filtered, estimation) in filtered_activities.items():
+    for activity, filtered, estimation in filtered_activities:
         chunks = estimation if estimation > 0 else ceil(len(filtered) / max_len)
         for chunk in np.array_split(filtered, chunks):
             x = signal.resample(chunk["x-axis"].values, max_len)
             y = signal.resample(chunk["y-axis"].values, max_len)
             z = signal.resample(chunk["z-axis"].values, max_len)
 
-            l_segments = np.vstack([l_segments, np.dstack([x, y, z])])
-            l_labels = np.append(l_labels, activity)
+            segments = np.vstack([segments, np.dstack([x, y, z])])
+            labels = np.append(labels, activity)
 
-    return l_segments, l_labels
+    return segments, labels
 
 
 length, activities = read_data_and_filter(args.input)
@@ -221,8 +220,6 @@ train_x = reshaped[train_test_split]
 train_y = labels[train_test_split]
 test_x = reshaped[~train_test_split]
 test_y = labels[~train_test_split]
-
-MODEL_NAME = "./" + args.output
 
 
 def weight_variable(shape):
@@ -251,7 +248,7 @@ def apply_max_pool(x, kernel_size, stride_size):
 
 
 ##############################################################################
-num_labels = len(activities)
+num_labels = 7
 num_channels = 3
 kernel_size = 60
 depth = 60
@@ -259,6 +256,7 @@ num_hidden = 1000
 training_epochs = 4
 batch_size = 10
 total_batches = train_x.shape[0] // batch_size
+OUTPUT_NAME = "O"
 ##############################################################################
 X = tf.placeholder(tf.float32, shape=[None, 1, length, num_channels], name="I")
 Y = tf.placeholder(tf.float32, shape=[None, num_labels])
@@ -272,12 +270,14 @@ f_biases_l1 = bias_variable([num_hidden])
 f = tf.nn.tanh(tf.add(tf.matmul(c_flat, f_weights_l1), f_biases_l1))
 out_weights = weight_variable([num_hidden, num_labels])
 out_biases = bias_variable([num_labels])
-y_ = tf.nn.softmax(tf.matmul(f, out_weights) + out_biases, name="O")
+y_ = tf.nn.softmax(tf.matmul(f, out_weights) + out_biases, name=OUTPUT_NAME)
 #############################################################################
 loss = -tf.reduce_sum(Y * tf.log(y_))
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0001).minimize(loss)
 correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+MODEL_NAME = "./" + args.output
 
 init_op = tf.global_variables_initializer()
 saver = tf.train.Saver()
@@ -310,7 +310,7 @@ input_graph_path = MODEL_NAME + '.pbtxt'
 checkpoint_path = MODEL_NAME + '.ckpt'
 input_saver_def_path = ""
 input_binary = False
-output_node_names = "O"
+output_node_names = OUTPUT_NAME
 restore_op_name = "save/restore_all"
 filename_tensor_name = "save/Const:0"
 output_frozen_graph_name = MODEL_NAME + '_frozen' + '.pb'
@@ -321,6 +321,7 @@ freeze_graph.freeze_graph(input_graph_path, input_saver_def_path, input_binary, 
 ##########
 # TFLITE #
 ##########
-converter = tf.lite.TFLiteConverter.from_frozen_graph(MODEL_NAME + '_frozen' + '.pb', ["I"], ["O"])
+converter = tf.lite.TFLiteConverter.from_frozen_graph(MODEL_NAME + '_frozen' + '.pb', ["I"],
+                                                      [OUTPUT_NAME])
 tflite_model = converter.convert()
 open(MODEL_NAME + "converted.tflite", "wb").write(tflite_model)
